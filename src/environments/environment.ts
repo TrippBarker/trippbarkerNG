9714d274966d47f81d5a6cc4fa17841e04279f8a
Tripp Barker <105750441+TrippBarker@users.noreply.github.com>
2024-03-23 11:46:18 -0400

    init application

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBMDG_5lDDr2YeB9GwJGP_C3XO5hdcltbk",
+  authDomain: "trippbarker-31e6e.firebaseapp.com",
+  projectId: "trippbarker-31e6e",
+  storageBucket: "trippbarker-31e6e.appspot.com",
+  messagingSenderId: "648512597814",
+  appId: "1:648512597814:web:25016cb4a291e39ee4cd97"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
